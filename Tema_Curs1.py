9798db959f9d23a1847b2350d77c22b10c1d4353
StoicaAndreiAlexandru <stoicaandreialexandru222@gmail.com>
2023-04-14 17:37:17 +0300

    Tema1

--- Tema_Curs1.py
+++ Tema_Curs1.py
@@ -1,4 +1,4 @@
-L = [7,8,9,2,3,1,4,10,5,6]
+L = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
 
 print(f"Lista initiala este {L}")
 ordonata = sorted(L)
@@ -7,7 +7,5 @@ print(f"Lista ordonata descendent este {ordonata[::-1]}")
 print(f"Lista numerelor cu indici pari este {L[::2]}")
 print(f"Lista numerelor cu indici impari este {L[1::2]}")
 
-print(f"Elementele multiple ale lui 3 sunt ", end="")
-for i in L:
-    if i % 3 == 0:
-        print(i, end=" ")
+Multiplii_3 = [i for i in L if (i % 3 == 0)]
+print(f"Elementele multiple ale lui 3 sunt {Multiplii_3} ", end="")
